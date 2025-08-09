ada7ac86f87498af7939d94caf438cc950470a74
Alexi Ramirez <rf0196022023@unab.edu.sv>
2025-08-09 10:22:10 -0600

    ruta cambiada

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('skills/', views.skills, name='skills'),
     path('projects/', views.projects, name='projects'),
 ]
